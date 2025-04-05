d9a59f6748893196ca3b8ef3ea14c727804728f9
Sergey Mescheryakov <mescheryakovsrg@gmail.com>
2025-04-05 21:15:57 +0300

    Add files via upload

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,83 @@
 // Copyright 2021 NNTU-CS
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int left = 0;
+  int right = len - 1;
+
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      if (arr[left] == arr[right]) {
+        int n = right - left + 1;
+        count += n * (n - 1) / 2;
+        break;
+      }
+      
+      int left_val = arr[left];
+      int left_count = 0;
+      while (left < len && arr[left] == left_val) {
+        left++;
+        left_count++;
+      }
+
+      int right_val = arr[right];
+      int right_count = 0;
+      while (right >= 0 && arr[right] == right_val) {
+        right--;
+        right_count++;
+      }
+
+      count += left_count * right_count;
+    } else if (sum < value) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    int target = value - arr[i];
+    int low = i + 1;
+    int high = len - 1;
+    
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] < target) {
+        low = mid + 1;
+      } else if (arr[mid] > target) {
+        high = mid - 1;
+      } else {
+        count++;
+        int left = mid - 1;
+        while (left > i && arr[left] == target) {
+          count++;
+          left--;
+        }
+        int right = mid + 1;
+        while (right < len && arr[right] == target) {
+          count++;
+          right++;
+        }
+        break;
+      }
+    }
+  }
+  return count;
 }
